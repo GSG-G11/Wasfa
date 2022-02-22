f02381e2faa309df83280afdbc564ac81c3579d0
farah2003 <farah.shaqoura@gmail.com>
2022-02-22 16:48:01 +0200

    add the recipeFile

--- src/controllers/recipe.js
+++ src/controllers/recipe.js
@@ -1,3 +1,10 @@
-const recipePage = (req, res) => {};
+const fetch = require('node-fetch');
+
+const recipePage = (req, res) => {
+  fetch('https://www.themealdb.com/api/json/v1/1/categoriess.php')
+    .then((response) => response.json())
+    .then((data) => res.json(data))
+    .catch((error) => res.send(`<h3>somethig went wrong with request<h3>`));
+};
 
 module.exports = recipePage;
