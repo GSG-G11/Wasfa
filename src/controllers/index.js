47b536412ead910d8074b7b523daecefe74e0ab2
Amjad Yahia <amjedyehia1998@gmail.com>
2022-02-22 19:20:35 +0200

    fixed few thing

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,12 +1,15 @@
 const recipeAPI = require('./recipeAPI');
-const searchRecipe = require('./searchRecipe');
+const searchRecipeAPI = require('./searchRecipeAPI');
 
 const handle404 = require('./404');
 const handle500 = require('./500');
 
 const handleRecipePage = require('./recipePage');
 
-module.exports = { searchRecipe, recipeAPI , handle404, handle500, handleRecipePage };
-
-
-
+module.exports = {
+  searchRecipeAPI,
+  recipeAPI,
+  handle404,
+  handle500,
+  handleRecipePage,
+};
